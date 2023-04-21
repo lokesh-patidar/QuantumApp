cbb387b8cbb051386b8f4964a77bf10437edb555
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-04-21 17:52:11 +0530

    Start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import HomePage from './Pages/HomePage';
+import Navbar from "./Pages/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+      <section id="home">
+        {/* <HomePage /> */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react';
+import '../Styles/Home.css';
+import Typical from "react-typical";
+
+const HomePage = () => {
+    return (
+        <Box id='Home'>
+            <Box>
+                <Box></Box>
+                <Box></Box>
+            </Box>
+            <Box></Box>
+        </Box>
+    );
+}
+
+export default HomePage;

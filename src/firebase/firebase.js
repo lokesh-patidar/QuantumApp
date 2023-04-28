15db75230e849e74ad23c475b9e45acc42e5a4ff
atul85400krishna <atul85400krishna@gmail.com>
2023-04-29 01:10:13 +0530

    website

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,33 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqTOuAon-0OFX1W0LzIpwe7eXEl3groqc",
-    authDomain: "lokeshauth-80775.firebaseapp.com",
-    projectId: "lokeshauth-80775",
-    storageBucket: "lokeshauth-80775.appspot.com",
-    messagingSenderId: "354286541740",
-    appId: "1:354286541740:web:267eb17dcf601c6d55f809",
-    measurementId: "G-FZDTZ8H1XR"
+     
+
+    // apiKey: "AIzaSyBgjwzguc3T279ENebbYKFYRdIJ1FfHBb8",
+    // authDomain: "quantum-tech.firebaseapp.com",
+    // // databaseURL: "https://quantum-tech-default-rtdb.firebaseio.com",
+    // projectId: "quantum-tech",
+    // storageBucket: "quantum-tech.appspot.com",
+    // messagingSenderId: "238526141371",
+    // appId: "1:238526141371:web:4cb0f647773f4a85e46a9e",
+    // measurementId: "G-Y5BX08CM2T"
+
+
+    apiKey: "AIzaSyBMRIwiSBdvCuwZJ7i60j5FyhaXqMNP0LU",
+    authDomain: "quantum-tech-ff4e8.firebaseapp.com",
+    projectId: "quantum-tech-ff4e8",
+    storageBucket: "quantum-tech-ff4e8.appspot.com",
+    messagingSenderId: "638490631504",
+    appId: "1:638490631504:web:4b51b2cefefc9160f4e055",
+    measurementId: "G-NNMBZCT19W"
+
+  //   apiKey: "AIzaSyDWNy0RwcadYzIS9oq1TOX3glqNF6BUVuY",
+  // authDomain: "ai-pedia-1676033385412.firebaseapp.com",
+  // projectId: "ai-pedia-1676033385412",
+  // storageBucket: "ai-pedia-1676033385412.appspot.com",
+  // messagingSenderId: "480125108716",
+  // appId: "1:480125108716:web:d9d0899111286a00fdc0c9",
+  // measurementId: "G-1Z2JNWG405"
 };
 
 firebase.initializeApp(firebaseConfig);
